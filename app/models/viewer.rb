fd16a531d17dc306c6010ee98cbcde892d3f45e4
colebuildanddevelop <colebuildanddevelop@gmail.com>
2020-04-29 10:30:05 -0500

    Cole -Davis

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,6 +1,5 @@
 class Viewer
   attr_accessor :username
-
   @@all = []
 
   def initialize(username)
@@ -11,5 +10,24 @@ class Viewer
   def self.all
     @@all
   end
-  
+
+  def reviews
+    Review.all.select { |r| r.viewer == self }
+  end
+
+  def reviewed_movies
+    self.reviews.map { |r| r.movie }
+  end
+
+  def reviewed_movie?(movie)
+    movie.reviewers.include?(self)
+  end
+
+  def rate_movie(movie, rating)
+    if !self.reviewed_movie?(movie)
+      Review.new(self, movie, rating)
+    else
+      self.reviews.find { |r| r.movie == movie }.rating = rating
+    end
+  end
 end
