1968d0534312ff714c418604f7ca913e94721bba
Eric Johnson <johnson.n.eric@gmail.com>
2020-04-29 10:31:24 -0500

    One down!

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -12,4 +12,20 @@ class Viewer
     @@all
   end
   
+  def reviews
+    Review.all.select{|rev|rev.viewer == self}
+  end
+
+  def reviewed_movies
+    reviews.map{|rev|rev.movie}
+  end
+
+  def reviewed_movie?(movie)
+    Review.all.find{|rev|rev.movie == movie && rev.viewer == self} ? true : false
+  end
+
+  def rate_movie(movie, rating)
+    reviewed_movie?(movie) ? Review.all.find{|rev|rev.movie == movie && rev.viewer == self}.rating = rating : Review.new(self, movie, rating) 
+  end
+
 end
