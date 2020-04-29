2cbc06935fcd27e83aa26759bff02f93ae56741f
devchea <rchea817@gmail.com>
2020-04-29 08:45:52 -0700

    done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,10 @@
+# Movie#average_rating
+# returns the average of all ratings for the Movie instance
+# to average ratings, add all ratings together and divide by the total number of ratings.
+
+# Movie.highest_rated
+# returns the Movie instance with the highest average rating.
+
 class Movie
   attr_accessor :title
 
@@ -12,4 +19,24 @@ class Movie
     @@all
   end
 
+  def reviews
+    Review.all.select {|review| review.movie == self}
+  end
+
+  def reviewers
+    reviews.map {|review| review.viewer}
+  end
+
+
+  def average_rating
+    watchers = @@all.count
+    x = Review.all.select {|review| review.movie == self}
+    total = x.collect {|review| review.rating}.sum.to_f
+    total/watchers
+  end
+
+  def self.highest_rated
+    #get average rating for each movie and return highest movie instance
+
+  end
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,3 +1,9 @@
+
+# Viewer#rate_movie(movie, rating)
+# a Movie instance and a rating (number) are passed in as arguments
+# if the Viewer instance and the passed Movie instance are not already associated, this method should create a new Review instance
+# if this Viewer has already reviewed this Movie, assigns the new rating to the existing Review instance
+
 class Viewer
   attr_accessor :username
 
@@ -11,5 +17,20 @@ class Viewer
   def self.all
     @@all
   end
+
+  def movies
+    Review.all.select {|review| review.viewer == self}
+  end
   
+  def movies_seen #show all movie instances seen by viewer
+    movies.map {|review| review.movie}
+  end
+
+  def reviewed_movie?(movie)
+    movies.include?(movie)
+  end
+
+  def rate_movie(movie, rating)
+    reviewed_movie?(movie) ? review.rating = rating : Review.new(self, movie, rating)
+  end
 end
