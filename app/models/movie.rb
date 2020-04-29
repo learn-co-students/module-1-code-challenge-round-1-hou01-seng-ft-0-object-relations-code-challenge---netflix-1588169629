cacb7cb797dc89d2615df5def753ea375c08025c
github username <github email address>
2020-04-29 10:44:08 -0500

    done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,18 @@ class Movie
     @@all
   end
 
+  def reviews
+    Review.all.select {|p| p.movie == self}
+  end
+
+  def reviewers
+    reviews.map {|p| p.viewer}
+  end
+
+  def average_rating
+   
+  end
+
+
+
 end
