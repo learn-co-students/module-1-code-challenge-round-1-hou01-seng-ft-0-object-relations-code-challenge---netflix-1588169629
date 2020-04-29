7ebd7651d6da17026cc38e7ff070ba2bc440602c
lbirts <birtslauren@gmail.com>
2020-04-29 10:38:45 -0500

    Done.

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,15 +1,54 @@
+# A viewer has many reviews
 class Viewer
+  # getter and setter method, allows username to be called and changed
   attr_accessor :username
 
+  # empty array that we will push viewer instances into
   @@all = []
 
+  # initializes viewer with username (string) and pushes instance into all array
   def initialize(username)
     @username = username
     self.class.all << self
   end
 
+  # returns an array of all viewer instances that have been initialized
   def self.all
     @@all
   end
-  
+
+  # returns an array of review instances associated with self
+  def reviews
+    Review.all.select {|rw| rw.viewer == self}
+  end
+
+  # returns an array of movie instances reivewed by viewer instance
+  def reviewed_movies
+    reviews.map {|rw| rw.movie}
+  end
+
+  # helper method that returns review instance instead of true/false
+  def movie_review(movie_inst)
+    reviews.find {|rw| rw.movie == movie_inst}
+  end
+
+  # takes a movie instance argument and returns true if viewer reviewed movie, otherwise return false
+  def reviewed_movie?(movie_inst)
+    if movie_review(movie_inst)
+      true
+    else
+      false
+    end
+    # reviewed_movies.any? {|mv| mv == movie_inst}
+  end
+
+  # takees a movie instance and rating and returns a new review IF viewer and movie are not already associated if they are then updates rating
+  def rate_movie(movie_inst, rating_num)
+    if movie_review(movie_inst)
+      movie_review(movie_inst).rating = rating_num
+    else
+      Review.new(self, movie_inst, rating_num)
+    end
+    movie_review(movie_inst)
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -10,8 +10,58 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+mv1 = Movie.new("All about the Benjamins")
+mv2 = Movie.new("Extraction")
+mv3 = Movie.new("The Whyllobys")
+mv4 = Movie.new("Little Fires Everywhere")
 
+vw1 = Viewer.new("Ava")
+vw2 = Viewer.new("Lauren")
+vw3 = Viewer.new("Kristie")
 
+rw1 = Review.new(vw2, mv1, 8)
+rw2 = Review.new(vw3, mv4, 9)
+rw3 = Review.new(vw1, mv3, 9)
+rw4 = Review.new(vw2, mv3, 8)
+rw5 = Review.new(vw3, mv2, 7)
+
+mv1.title
+# returns "All about the Benjamins"
+Movie.all 
+
+vw2.username
+# returns "Lauren"
+Viewer.all
+
+rw1.rating
+# returns 8
+rw2.viewer 
+# returns Kristie viewer instance
+rw2.movie
+# returns little fires everywhere movie instance
+Review.all
+
+vw2.reviews
+vw2.reviewed_movies
+
+mv3.reviews
+mv3.reviewers
+
+vw1.reviewed_movie?(mv2) 
+# returns false
+vw1.reviewed_movie?(mv3)
+# returns true
+
+vw3.rate_movie(mv1, 5)
+# returns new review instance
+vw3.rate_movie(mv4, 10)
+# returns updated review instance
+vw3.reviews
+
+mv3.average_rating
+# returns 8.5
+Movie.highest_rated
+# returns little fires everywhere instance
 
 
 # ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
